d6366ec16315cd6ff2ba3222cc5ad54ed4fc5563
Ian Gacek <gacek004@umn.edu>
2019-04-27 19:25:04 -0500

    js updates

---
+++ assets/javascript/game.js
@@ -0,0 +1,31 @@
+var letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+var wins = 0;
+var losses = 0;
+var guessesRemaining = 10;
+var guesses = "";
+var playerLetter = "";
+
+// Calculating random number
+var cpuChoice = letters[Math.floor(Math.random() * 2)];
+// Confirms letter is picked correctly
+console.log(cpuChoice);
+
+// Captures user letter input
+document.onkeydown = function(event) {
+    var playerLetter = event.key;
+    console.log(playerLetter);
+}
+
+// I have no idea why this isn't working. It should work... I think?
+if (playerLetter === cpuChoice) {
+    wins++;
+    alert("You've done it!");
+} else if (playerLetter !== cpuChoice) {
+    guessesRemaining--;
+    '#guesses'.append(playerLetter);
+}
+
+if (guessesRemaining === 0) {
+    losses++;
+    alert("You lose!");
+}
